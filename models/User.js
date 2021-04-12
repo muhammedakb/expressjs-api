9605ab51a0cf20defc6d9dcdfe0874489d01cfdd
muhammedakb <muhammetakb68@gmail.com>
2021-04-12 23:24:34 +0300

    🎉

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
+const Question = require("./Athlete");
 
 const UserSchema = new Schema({
   name: {
@@ -106,6 +107,13 @@ UserSchema.pre("save", function (next) {
       this.password = hash;
       next();
     });
+  });
+});
+
+// Bir kullanıcı silindiğinde, onun sorularıda silinecek
+UserSchema.post("remove", async function () {
+  await Question.deleteMany({
+    user: this._id,
   });
 });
 
