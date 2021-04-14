772ad93a9a087775194eefacfaa8f36e398b2b3f
muhammedakb <muhammetakb68@gmail.com>
2021-04-14 13:07:17 +0300

    🎉

--- routers/question.js
+++ routers/question.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const answer = require("./answer");
 const {
   getSingleQuestion,
   getAllQuestions,
@@ -37,5 +38,7 @@ router.delete(
   [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess],
   deleteQuestion
 );
+
+router.use("/:question_id/answers", checkQuestionExist, answer);
 
 module.exports = router;
